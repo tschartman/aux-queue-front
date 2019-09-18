f76b5c467d0c90c9de6564cd5b9cb1b8f36811b2
Tim Schartman <tschartman@yahoo.com>
2019-09-18 00:40:24 -0500

    added missing keyword

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-express = require('express'),
-serveStatic = require('serve-static'),
-history = require('connect-history-api-fallback'),
-port = process.env.PORT || 5000
+const
+    express = require('express'),
+    serveStatic = require('serve-static'),
+    history = require('connect-history-api-fallback'),
+    port = process.env.PORT || 5000
 
 const app = express()
 
