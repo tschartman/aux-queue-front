563afb5b3f37cd6b7c2e0fc27b015968a1d9b30f
tim <tschartman@yahoo.com>
2020-02-22 02:47:44 -0600

    switch from artists and tracks

--- src/pages/user/User.vue
+++ src/pages/user/User.vue
@@ -72,10 +72,29 @@
     </div>
     <q-separator />
     <div class="row justify-center">
-      <h5>Top Spotify Artists</h5>
+      <div class="q-mx-lg">
+        <h5>Top Spotify {{ category }}</h5>
+      </div>
+      <div>
+        <q-btn
+          v-if="category === 'Artists'"
+          round
+          @click="category = 'Tracks'"
+          style="height: 50px"
+          icon="art_track"
+        />
+        <q-btn
+          v-else
+          round
+          @click="category = 'Artists'"
+          style="height: 50px"
+          icon="face"
+        />
+      </div>
     </div>
     <div class="row justify-center">
       <q-carousel
+        v-if="category === 'Artists'"
         v-model="slide"
         transition-prev="slide-right"
         transition-next="slide-left"
@@ -101,6 +120,37 @@
               v-for="artist in matrix.row"
               :src="artist.images[0].url"
               v-bind:key="artist.id"
+            />
+          </div>
+        </q-carousel-slide>
+      </q-carousel>
+      <q-carousel
+        v-else
+        v-model="slide"
+        transition-prev="slide-right"
+        transition-next="slide-left"
+        swipeable
+        animated
+        control-color="primary"
+        navigation
+        padding
+        arrows
+        style="width:500px"
+        height="475px"
+        class="rounded-borders"
+      >
+        <q-carousel-slide
+          v-for="(matrix, index) in trackMatrix"
+          :name="index"
+          v-bind:key="index"
+        >
+          <div class="row">
+            <q-img
+              class="col-6"
+              ratio="1"
+              v-for="track in matrix.row"
+              :src="track.album.images[0].url"
+              v-bind:key="track.id"
             />
           </div>
         </q-carousel-slide>
@@ -138,6 +188,7 @@ export default {
     return {
       slide: 1,
       user: {},
+      category: "Artists",
       artistMatrix: [],
       friends: [],
       imageUrl: "",
@@ -159,7 +210,7 @@ export default {
       for (i = 0, j = array.length; i < j; i += chunk) {
         templist.push({ row: array.slice(i, i + chunk) });
       }
-      this.artistMatrix = Array.from(templist);
+      return templist;
     },
     filterFn(val, update, abort) {
       if (val.length < 1) {
@@ -193,8 +244,10 @@ export default {
       query: USER_DATA_QUERY
     });
 
-    const spotifyData = await spotify_api.get("/me/top/artists");
-    this.splitArray(spotifyData.data.items);
+    const artists = await spotify_api.get("/me/top/artists");
+    const tracks = await spotify_api.get("/me/top/tracks");
+    this.artistMatrix = this.splitArray(artists.data.items);
+    this.trackMatrix = this.splitArray(tracks.data.items);
     this.user = userData.data.user;
     this.imageUrl = "https://www.gravatar.com/avatar/" + md5(this.user.email);
   }
